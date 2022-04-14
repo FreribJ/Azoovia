7f3a76b1c824c09aa65417aa7d9516b3d205d393
Jannes <frerib.j@gmail.com>
2022-04-14 16:20:10 +0200

    moved files

---
+++ Java/generateValues.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class generateValues {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public static String Animal() {
+        //insert into "Animal"("idAnimal","zoo_id", "enclosure_id", "species_id","name", "age", "sex") values (1,1,4,5, 'Leon', 20, 'male');
+        return "hallo";
+    }
+}

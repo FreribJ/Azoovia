6abfae0676ca6412ac89661ccb3b220cd1e06c8c
jan1310 <jan.schiffer@hsw-stud.de>
2022-04-14 15:50:38 +0200

    jan

--- Initialisierung.sql
+++ Initialisierung.sql
@@ -1,49 +1,51 @@
 create table "Animal"
 (
     "idAnimal"     serial primary key,
-    "zoo_id"       integer REFERENCES "Zoo" ("idZoo"),
-    "enclosure_id" integer REFERENCES "Enclosure" ("idEnclosure"),
-    "species_id"   integer REFERENCES "Species" ("idSpecies"),
+    "zoo_id"       integer REFERENCES "Zoo" ("idZoo") not null,
+    "enclosure_id" integer REFERENCES "Enclosure" ("idEnclosure") not null,
+    "species_id"   integer REFERENCES "Species" ("idSpecies") not null,
     "name"         varchar(255),
-    "age"          integer,
-    "sex"          varchar(1)
+    "age"          integer not null,
+    "sex"          varchar(1) not null
 );
 
 create table "Habitat"
 (
     "idHabitat" serial primary key,
-    "name"      varchar(255)
+    "name"      varchar(255) not null
 );
 
 create table "Enclosure"
 (
     "idEnclosure" serial primary key,
     "habitat_id"  integer references "Habitat" ("idHabitat"),
-    "area"        integer
+    "area"        integer not null
 );
 
 create table "Species"
 (
     "idSpecies"  serial primary key,
     "habitat_id" integer references "Habitat" ("idHabitat"),
-    "name"       varchar(255)
+    "name"       varchar(255) not null
 );
 
 create table "FeedingPlan"
 (
     "idFeedingPlan" serial primary key,
-    "animal_id"     integer references "Habitat" ("idHabitat"),
+    "animal_id"     integer references "Animal" ("idAnimal"),
     "feed_id"       integer references "Feed" ("idFeed"),
     "employee_id"   integer references "Employee" ("idEmployee"),
-    "time"          time,
-    "amount"        integer
+    "time"          time not null,
+    "amount"        integer not null
 );
+
+create unique index feeding_plan_unique on "FeedingPlan" (animal_id, feed_id, employee_id, time);
 
 create table "Feed"
 (
     "idFeed" serial primary key,
-    "name"   varchar(255),
-    "price"  integer
+    "name"   varchar(255) not null,
+    "price"  integer not null
 );
 
 create table "Employee"
@@ -51,47 +53,48 @@ create table "Employee"
     "idEmployee" serial primary key,
     "zoo_id"     integer references "Zoo" ("idZoo"),
     "job_id"     integer references "Job" ("idJob"),
-    "firstname"  varchar(255),
-    "lastname"   varchar(255),
-    "address"    varchar(255),
-    "email"      varchar(255),
-    "age"        integer,
-    "hireDate"   date
+    "firstname"  varchar(255) not null,
+    "lastname"   varchar(255) not null,
+    "address"    varchar(255) not null,
+    "email"      varchar(255) not null,
+    "age"        integer not null,
+    "hireDate"   date not null
 );
 
 create table "Job"
 (
     "idJob"  serial primary key,
-    "name"   varchar(255),
-    "salary" integer
+    "name"   varchar(255) not null,
+    "salary" integer not null
 );
 
 create table "Zoo"
 (
     "idZoo"   serial primary key,
-    "name"    varchar(255),
-    "address" varchar(255)
+    "name"    varchar(255) not null,
+    "address" varchar(255) not null
 );
 
 create table "Ticket"
 (
     "idTicket"      serial primary key,
-    "visitor_id"    varchar(255),
+    "visitor_id"    integer references "Visitor" ("idVisitor"),
     "zoo_id"        integer references "Zoo" ("idZoo"),
-    "ticketType_id" integer references "TicketType" ("idTicketType")
+    "ticketType_id" integer references "TicketType" ("idTicketType"),
+    "validityDate"  date not null
 );
 
 create table "TicketType"
 (
     "idTicketType" serial primary key,
-    "name"         varchar(255),
-    "price"        integer
+    "name"         varchar(255) not null,
+    "price"        integer not null
 );
 
 create table "Visitor"
 (
     "idVisitor" serial primary key,
-    "firstname" varchar(255),
-    "lastname"  varchar(255),
-    "age"       integer
+    "firstname" varchar(255) not null,
+    "lastname"  varchar(255) not null,
+    "age"       integer not null
 );

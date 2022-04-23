f6caf473e5800dc9b2c365aa52b09fd913a63d6c
jan1310 <jan.schiffer@hsw-stud.de>
2022-04-23 13:44:28 +0200

    jan

--- Abfragen-Jan.sql
+++ Abfragen-Jan.sql
@@ -4,3 +4,10 @@ JOIN "Animal" A on "Enclosure"."idEnclosure" = A.enclosure_id
 JOIN "FeedingPlan" FP on A."idAnimal" = FP.animal_id
 GROUP BY "idEnclosure"
 ORDER BY "idEnclosure";
+
+--Durchschnitt des Essen pro Spezie
+SELECT S.name, round(avg(amount), 2) AS average FROM "FeedingPlan"
+JOIN "Animal" A on A."idAnimal" = "FeedingPlan".animal_id
+JOIN "Species" S on S."idSpecies" = A.species_id
+GROUP BY S.name
+ORDER BY average DESC;

1d808265caca3b5d4de927c142807227922fd8e3
RealMaggi <buxmag@hotmail.com>
2022-04-27 14:11:55 +0200

    Magnus

--- AbfragenMagnus.sql
+++ AbfragenMagnus.sql
@@ -1,10 +1,10 @@
 -- Artgerechtheit
-SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS Artgerecht
-from "Animal" A
+SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS SpeciesAppropriate
+FROM "Animal" A
 JOIN "Enclosure" E on E."idEnclosure" = A.enclosure_id
-Join "Habitat" H1 ON E.habitat_id = H1."idHabitat"
+JOIN "Habitat" H1 ON E.habitat_id = H1."idHabitat"
 JOIN "Species" S on S."idSpecies" = A.species_id
-join "Habitat" H2 ON S.habitat_id = H2."idHabitat"
+JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat"
 
-
+-- Der Artengerechteste Zoo
 

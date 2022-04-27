33914389ad4793d4da6ef1efc163a38a80ce5837
RealMaggi <buxmag@hotmail.com>
2022-04-27 15:27:53 +0200

    Magnus

--- AbfragenMagnus.sql
+++ AbfragenMagnus.sql
@@ -6,7 +6,7 @@ JOIN "Habitat" H1 ON E.habitat_id = H1."idHabitat"
 JOIN "Species" S ON S."idSpecies" = A.species_id
 JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat";
 
--- Der Artengerechteste Zoo
+-- Artgerechtheit
 SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS SpeciesAppropriate, A.zoo_id
 FROM "Animal" A
 JOIN "Enclosure" E ON E."idEnclosure" = A.enclosure_id
@@ -15,8 +15,8 @@ JOIN "Species" S ON S."idSpecies" = A.species_id
 JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat"
 WHERE H1."idHabitat" = H2."idHabitat";
 
--- testtry
-SELECT Z."name", count(zoo_id) AS Artengerecht_Gehaltene_Tiere
+-- Der artengerechteste Zoo
+SELECT Z."name", count(zoo_id) AS Species_appropriately_held_animals
 FROM "Animal" A
 JOIN "Enclosure" E ON E."idEnclosure" = A.enclosure_id
 JOIN "Habitat" H1 ON E.habitat_id = H1."idHabitat"
@@ -25,7 +25,7 @@ JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat"
 JOIN "Zoo" Z on A.zoo_id = Z."idZoo"
 WHERE H1."idHabitat" = H2."idHabitat"
 group by Z."name"
-ORDER BY Artengerecht_Gehaltene_Tiere ASC;
+ORDER BY Species_appropriately_held_animals ASC;
 
 
 

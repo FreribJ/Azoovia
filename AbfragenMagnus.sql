d2da19f41b6370b98d309d6e7a244c5a3d4cb67c
RealMaggi <buxmag@hotmail.com>
2022-04-27 14:09:55 +0200

    Magnus

--- AbfragenMagnus.sql
+++ AbfragenMagnus.sql
@@ -1,7 +1,10 @@
 -- Artgerechtheit
-SELECT "idAnimal"
-from "Species", "Enclosure", "Animal"
-where "Animal"."enclosure_id"
-AND "Enclosure"."habitat_id" = "Species"."habitat_id"
-group by "idAnimal"
-order by "idAnimal" ASC;
+SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS Artgerecht
+from "Animal" A
+JOIN "Enclosure" E on E."idEnclosure" = A.enclosure_id
+Join "Habitat" H1 ON E.habitat_id = H1."idHabitat"
+JOIN "Species" S on S."idSpecies" = A.species_id
+join "Habitat" H2 ON S.habitat_id = H2."idHabitat"
+
+
+

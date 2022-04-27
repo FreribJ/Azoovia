f9180e31cc5f52a4f2ef5779d98caa37b0666e1f
RealMaggi <buxmag@hotmail.com>
2022-04-27 15:04:27 +0200

    Magnus

--- AbfragenMagnus.sql
+++ AbfragenMagnus.sql
@@ -7,7 +7,7 @@ JOIN "Species" S ON S."idSpecies" = A.species_id
 JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat";
 
 -- Der Artengerechteste Zoo
-SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS SpeciesAppropriate
+SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS SpeciesAppropriate, A.zoo_id
 FROM "Animal" A
 JOIN "Enclosure" E ON E."idEnclosure" = A.enclosure_id
 JOIN "Habitat" H1 ON E.habitat_id = H1."idHabitat"

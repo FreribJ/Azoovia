5d93f65252a06faa1be075a225c108baaac478ca
RealMaggi <buxmag@hotmail.com>
2022-04-27 15:26:09 +0200

    Magnus

--- AbfragenMagnus.sql
+++ AbfragenMagnus.sql
@@ -15,5 +15,17 @@ JOIN "Species" S ON S."idSpecies" = A.species_id
 JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat"
 WHERE H1."idHabitat" = H2."idHabitat";
 
+-- testtry
+SELECT Z."name", count(zoo_id) AS Artengerecht_Gehaltene_Tiere
+FROM "Animal" A
+JOIN "Enclosure" E ON E."idEnclosure" = A.enclosure_id
+JOIN "Habitat" H1 ON E.habitat_id = H1."idHabitat"
+JOIN "Species" S ON S."idSpecies" = A.species_id
+JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat"
+JOIN "Zoo" Z on A.zoo_id = Z."idZoo"
+WHERE H1."idHabitat" = H2."idHabitat"
+group by Z."name"
+ORDER BY Artengerecht_Gehaltene_Tiere ASC;
+
 
 

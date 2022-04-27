8a205f53992a75214427f8a88a729e3b26de94eb
RealMaggi <buxmag@hotmail.com>
2022-04-27 15:34:49 +0200

    Magnus

--- AbfragenMagnus.sql
+++ AbfragenMagnus.sql
@@ -6,7 +6,7 @@ JOIN "Habitat" H1 ON E.habitat_id = H1."idHabitat"
 JOIN "Species" S ON S."idSpecies" = A.species_id
 JOIN "Habitat" H2 ON S.habitat_id = H2."idHabitat";
 
--- Artgerechtheit
+-- Artgerechtheit II
 SELECT A."name", H1."name" AS EnclosureHabitat, H2."name" AS SpeciesHabitat, H1."idHabitat" = H2."idHabitat" AS SpeciesAppropriate, A.zoo_id
 FROM "Animal" A
 JOIN "Enclosure" E ON E."idEnclosure" = A.enclosure_id
